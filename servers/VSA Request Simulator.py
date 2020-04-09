2db5163ca60da4e66aa0223d29d272ca9fac3000
Michael Yuwono <michael@codeheroes.com.au>
2020-04-09 11:48:17 +1000

    chore: refactor python scripts

--- servers/VSA Request Simulator.py
+++ servers/VSA Request Simulator.py
@@ -1,33 +1,33 @@
 '''
 Developed for Vehicle Situational Awareness Project
-Copyright © Queensland University of Technology 2019
+Copyright © Queensland University of Technology 2020
 Authored by @yuwonom (Michael Yuwono)
 '''
 import paho.mqtt.client as mqtt
 import VSA, Broker
 
 #properties
-NAME = "VSA Request Simulator";
-VERSION = "2.0.1";
+NAME = "VSA Request Simulator"
+VERSION = "2.0.2"
 
 # ------------------------------------------------------------------------ #
 
-print("===== " + NAME + " v" + VERSION + " =====");
+print("===== " + NAME + " v" + VERSION + " =====")
 
 #new client
-client = mqtt.Client();
-client.username_pw_set(Broker.USERNAME, Broker.PASSWORD);
-client.connect(Broker.ADDRESS, Broker.PORT);
-print("Connected to broker.");
+client = mqtt.Client()
+client.username_pw_set(Broker.USERNAME, Broker.PASSWORD)
+client.connect(Broker.ADDRESS, Broker.PORT)
+print("Connected to broker.")
 
-main_topic = VSA.TOPIC_TRAFFIC_NEARBY_REQ + "/1";
+main_topic = VSA.TOPIC_TRAFFIC_NEARBY_REQ + "/1"
 
 try:
 	while True:
-		print("publishing to topic ==> " + main_topic);
-		message = input("Enter your message: ");
-		client.publish(main_topic, message);
-		print("message successfully published");
+		print("publishing to topic ==> " + main_topic)
+		message = input("Enter your message: ")
+		client.publish(main_topic, message)
+		print("message successfully published")
 	
 except KeyboardInterrupt:
-	client.disconnect();
+	client.disconnect()
