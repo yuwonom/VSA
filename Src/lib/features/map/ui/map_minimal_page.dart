7391a79d6b68205d5c5bdc66e461c55361cbaea6
Michael Yuwono <michael@codeheroes.com.au>
2020-05-04 14:36:53 +1000

    feature: add speed indicator

--- Src/lib/features/map/ui/map_minimal_page.dart
+++ Src/lib/features/map/ui/map_minimal_page.dart
@@ -74,39 +74,196 @@ class _CarPageState extends State<_CarPage> {
       child: _buildMap(store, viewModel),
     );
 
-    final actionButtons = Padding(
-      padding: AppEdges.mediumAll,
-      child: _buildActionButtons(store, viewModel),
+    final topView = Align(
+      alignment: Alignment.topCenter,
+      child: _buildTopBar(viewModel),
     );
 
-    final topView = _buildTopBar(viewModel);
-
-    final bottomView = Wrap(
-      alignment: WrapAlignment.center,
-      children: <Widget>[
-        actionButtons,
-        _buildBottomBar(viewModel),
-      ],
+    final bottomView = Align(
+      alignment: Alignment.bottomCenter,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _buildSpeedIndicator(viewModel),
+          _buildBottomBar(store, viewModel),
+        ]
+      ),
     );
 
     final page = Stack(
       children: <Widget>[
         map,
-        Align(
-          alignment: Alignment.topCenter,
-          child: topView,
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: bottomView,
-        ),
+        topView,
+        bottomView,
       ],
     );
 
     return page;
   }
 
-  Widget _buildActionButtons(Store<AppState> store, MapViewModel viewModel) {
+  Widget _buildTopBar(MapViewModel viewModel) {
+    Color barColor = AppColors.gray;
+    Color textColor = AppColors.darkGray;
+    String text = "Not connected to any broker service.\nPress start to connect.";
+
+    if (viewModel.connectionState == MqttConnectionState.connected) {
+      textColor = AppColors.white;
+      switch (viewModel.securityLevel) {
+        case (SecurityLevelDto.secured):
+          barColor = AppColors.green;
+          text = "Connected to broker service.\nPlease drive safely";
+          break;
+        case (SecurityLevelDto.controlled):
+          barColor = AppColors.lightGreen;
+          text = "Connected to broker service.\nPlease drive safely";
+          break;
+        case (SecurityLevelDto.cautious):
+          barColor = AppColors.yellow;
+          text = "There's a bike approaching.\nPlease drive with cautious.";
+          break;
+        case (SecurityLevelDto.dangerous):
+          barColor = AppColors.orange;
+          text = "Warning! Possible collision detected.";
+          break;
+        case (SecurityLevelDto.critical):
+          barColor = AppColors.red;
+          text = "Warning! Possible collision detected.";
+          break;
+        case (SecurityLevelDto.unknown):
+          barColor = AppColors.gray;
+          textColor = AppColors.darkGray;
+          text = "Unknown security level.";
+          break;
+      }
+    } else if (viewModel.connectionState == MqttConnectionState.connecting) {
+      barColor = AppColors.gray;
+      textColor = AppColors.darkGray;
+      text = "Connecting...";
+    } else if (viewModel.connectionState == MqttConnectionState.disconnecting) {
+      barColor = AppColors.gray;
+      textColor = AppColors.darkGray;
+      text = "Disconnecting...";
+    }
+
+    final topBar = Container(
+      color: barColor,
+      padding: AppEdges.mediumAll,
+      width: MediaQuery.of(context).size.width,
+      child: Text(
+        text,
+        style: AppTextStyles.body1.copyWith(color: textColor),
+        textAlign: TextAlign.center,
+      ),
+    );
+
+    return topBar;
+  }
+
+  Widget _buildBottomBar(Store<AppState> store, MapViewModel viewModel) {
+    final inIntersectionText = Wrap(
+      children: <Widget>[
+        Text("You are currently in intersection ",
+          style: AppTextStyles.body2.copyWith(color: AppColors.black)),
+        Text("#${viewModel.currentIntersectionId}",
+          style: AppTextStyles.body2.copyWith(
+            color: AppColors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+
+    final totalVehicles = Text(
+      "Found ${viewModel.otherVehicles.length} active vehicles",
+      style: AppTextStyles.body2.copyWith(color: AppColors.black),
+    );
+
+    final noIntersectionText = Text(
+      "You are not currently within any intersection",
+      style: AppTextStyles.body2.copyWith(color: AppColors.black),
+    );
+
+    final intersectionText = viewModel.currentIntersectionId != null
+      ? Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            inIntersectionText,
+            SizedBox(height: AppLengths.tiny),
+            totalVehicles,
+          ],
+        )
+      : noIntersectionText;
+    
+    final contents = Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Expanded(child: intersectionText),
+        SizedBox(width: AppLengths.small),
+        _buildPlayButton(store, viewModel),
+      ],
+    );
+
+    final bottomBar = Container(
+      color: AppColors.white,
+      padding: AppEdges.smallAll,
+      width: MediaQuery.of(context).size.width,
+      child: contents,
+    );
+
+    return bottomBar;
+  }
+
+  Widget _buildSpeedIndicator(MapViewModel viewModel) {
+    final speedText = Text(
+      viewModel.userVehicle.point?.speed?.toStringAsFixed(0) ?? "0",
+      style: AppTextStyles.header1.copyWith(
+        color: AppColors.black,
+        fontSize: 28,
+        height: 1,
+      ),
+    );
+
+    final kph = Text(
+      "KPH",
+      style: AppTextStyles.caption.copyWith(
+        color: AppColors.black,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+
+    final container = Container(
+      decoration: BoxDecoration(
+        border: Border.all(width: 2.0),
+        borderRadius: AppBorders.bezelGeom,
+        color: AppColors.white,
+      ),
+      padding: AppEdges.tinyAll,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          speedText,
+          kph,
+        ],
+      ),
+    );
+
+    final card = Card(
+      color: AppColors.white,
+      elevation: 3.0,
+      margin: AppEdges.smallAll,
+      shape: AppBorders.bezel,
+      child: Padding(
+        padding: AppEdges.tinyAll,
+        child: container,
+      ),
+    );
+
+    return card;
+  }
+
+  Widget _buildPlayButton(Store<AppState> store, MapViewModel viewModel) {
     Widget playButton;
 
     final VoidCallback connectCallback = () {
@@ -171,109 +328,6 @@ class _CarPageState extends State<_CarPage> {
     }
 
     return playButton;
-  }
-
-  Widget _buildTopBar(MapViewModel viewModel) {
-    Color barColor = AppColors.gray;
-    Color textColor = AppColors.darkGray;
-    String text = "Not connected to any broker service.\nPress start to connect.";
-
-    if (viewModel.connectionState == MqttConnectionState.connected) {
-      textColor = AppColors.white;
-      switch (viewModel.securityLevel) {
-        case (SecurityLevelDto.secured):
-          barColor = AppColors.green;
-          text = "Connected to broker service.\nPlease drive safely";
-          break;
-        case (SecurityLevelDto.controlled):
-          barColor = AppColors.lightGreen;
-          text = "Connected to broker service.\nPlease drive safely";
-          break;
-        case (SecurityLevelDto.cautious):
-          barColor = AppColors.yellow;
-          text = "There's a bike approaching.\nPlease drive with cautious.";
-          break;
-        case (SecurityLevelDto.dangerous):
-          barColor = AppColors.orange;
-          text = "Warning! Possible collision detected.";
-          break;
-        case (SecurityLevelDto.critical):
-          barColor = AppColors.red;
-          text = "Warning! Possible collision detected.";
-          break;
-        case (SecurityLevelDto.unknown):
-          barColor = AppColors.gray;
-          textColor = AppColors.darkGray;
-          text = "Unknown security level.";
-          break;
-      }
-    } else if (viewModel.connectionState == MqttConnectionState.connecting) {
-      barColor = AppColors.gray;
-      textColor = AppColors.darkGray;
-      text = "Connecting...";
-    } else if (viewModel.connectionState == MqttConnectionState.disconnecting) {
-      barColor = AppColors.gray;
-      textColor = AppColors.darkGray;
-      text = "Disconnecting...";
-    }
-
-    final topBar = Container(
-      color: barColor,
-      padding: AppEdges.mediumAll,
-      width: MediaQuery.of(context).size.width,
-      child: Text(
-        text,
-        style: AppTextStyles.body1.copyWith(color: textColor),
-        textAlign: TextAlign.center,
-      ),
-    );
-
-    return topBar;
-  }
-
-  Widget _buildBottomBar(MapViewModel viewModel) {
-    final inIntersectionText = Wrap(
-      children: <Widget>[
-        Text("You are currently in intersection ",
-          style: AppTextStyles.body1.copyWith(color: AppColors.black)),
-        Text("#${viewModel.currentIntersectionId}",
-          style: AppTextStyles.body1.copyWith(
-            color: AppColors.black,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ],
-    );
-
-    final totalVehicles = Text(
-      "Found ${viewModel.otherVehicles.length} vehicles",
-      style: AppTextStyles.body1.copyWith(color: AppColors.black),
-    );
-
-    final noIntersectionText = Text(
-      "You are not currently within any intersection",
-      style: AppTextStyles.body2.copyWith(color: AppColors.black),
-    );
-
-    final intersectionText = viewModel.currentIntersectionId != null
-      ? Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            inIntersectionText,
-            SizedBox(height: AppLengths.tiny),
-            totalVehicles,
-          ],
-        )
-      : noIntersectionText;
-
-    final bottomBar = Container(
-      color: AppColors.white,
-      padding: AppEdges.mediumAll,
-      width: MediaQuery.of(context).size.width,
-      child: intersectionText,
-    );
-
-    return bottomBar;
   }
 
   Widget _buildMap(Store<AppState> store, MapViewModel viewModel) {
